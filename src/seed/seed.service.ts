1fb9c764efa7ab07124efddf5aabc321d7a43b98
Mac Mini 250 <macmini250@Daniel.local>
2022-08-07 18:06:43 -0500

    before load adata

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import axios, {AxiosInstance} from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+  
+  async executeSeed(){
+    const {data}=await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=3');
+    data.results.forEach(({name, url})=>{
+
+      const segments =  url.split("/");
+      const no:number = +segments?.[6];
+
+      console.log(name, no);
+    
+    } )
+    return data.results;
+  }
+
+}
+
+
