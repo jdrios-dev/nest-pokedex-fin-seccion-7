080215efe2569666a6f08954896cdcc7f5495abf
Mac Mini 250 <macmini250@Daniel.local>
2022-08-07 18:19:14 -0500

    inyect data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import axios, {AxiosInstance} from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from '../pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    private readonly pokemonService: PokemonService,
+  ){}
   
   async executeSeed(){
     const {data}=await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=3');
@@ -15,6 +19,7 @@ export class SeedService {
       const no:number = +segments?.[6];
 
       console.log(name, no);
+      this.pokemonService.create({no, name});
     
     } )
     return data.results;
